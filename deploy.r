86bc2c811cdb69f76e408a457fe94bb86ded4930
Łukasz <107052397+lucapl@users.noreply.github.com>
2023-06-12 11:16:29 +0200

    update deploy script

--- deploy.r
+++ deploy.r
@@ -3,4 +3,4 @@ setAccountInfo(name = Sys.getenv("SHINY_ACC_NAME"),
                token = Sys.getenv("TOKEN"),
                secret = Sys.getenv("SECRET"))
 # Deploy
-deployApp(appFiles = c("ui.R", "server.R", "global.r"),appName="Weather-Report")
+deployApp(appFiles = c("ui.R", "server.R", "global.r","www/","data/","README.md"),appName="Weather-Report")
